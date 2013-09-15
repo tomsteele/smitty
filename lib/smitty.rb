09793b406650a77b22fe49d58e2911c56a5cdce4
tomsteele <thomasjsteele@gmail.com>
2013-09-15 14:34:44 -0700

    Require new files

--- lib/smitty.rb
+++ lib/smitty.rb
@@ -1,8 +1,5 @@
 require 'smitty/version'
 require 'smitty/usage'
 require 'smitty/croak'
-
-Smitty::usage
-module Smitty
-  # Your code goes here...
-end
+require 'smitty/variable_parser'
+require 'smitty/smtp_connect'
