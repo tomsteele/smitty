615da2c9899857004898fa8ccab2103300018372
tomsteele <thomasjsteele@gmail.com>
2013-09-15 13:58:11 -0700

    Fixes spacing

--- lib/smitty/croak.rb
+++ lib/smitty/croak.rb
@@ -1,5 +1,5 @@
 module Smitty
-# Prints variable amount of messages and exits
+  # Prints variable amount of messages and exits
   def self.croak(*messages)
     messages.each { |message| puts "Error: #{message}"}
     exit(1)
