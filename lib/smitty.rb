d012505fe8bc4fc55f3f1b2d286c7804f7f99ac7
tomsteele <thomasjsteele@gmail.com>
2013-09-15 13:57:34 -0700

    Move error handling into separate file

--- lib/smitty.rb
+++ lib/smitty.rb
@@ -1,5 +1,7 @@
 require 'smitty/version'
 require 'smitty/usage'
+require 'smitty/croak'
+
 Smitty::usage
 module Smitty
   # Your code goes here...
---
+++ lib/smitty/croak.rb
@@ -0,0 +1,7 @@
+module Smitty
+# Prints variable amount of messages and exits
+  def self.croak(*messages)
+    messages.each { |message| puts "Error: #{message}"}
+    exit(1)
+  end
+end
