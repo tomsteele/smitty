bc3e7b75e6ed475dc57f6312202f20defc38f56f
tomsteele <thomasjsteele@gmail.com>
2013-09-16 12:33:52 -0700

    Fixes variable length checking

--- lib/smitty/variable_parser.rb
+++ lib/smitty/variable_parser.rb
@@ -1,9 +1,10 @@
 module Smitty
-  def self.variable_parser(variable_string)
+  def self.variable_parser(variable_string, mail_length)
     pairs = variable_string.split('=')
     # content is a file
     begin
       with_content = File.readlines(pairs[1]).map(&:chomp)
+      Smitty.croak("#{pairs[0]} != recipient length") if with_content.length != mail_length
     rescue Errno::ENOENT
       with_content = pairs[1]
     rescue
